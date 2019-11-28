f51153825ddba849dd98b5b44c50446e0df1e6b4
burakberber <burak@burakberber.net>
2019-11-28 20:48:32 +0300

    Add files via upload

---
+++ objects-references.js
@@ -0,0 +1,36 @@
+let myAccount={
+    name: 'burakberber',
+    expenses: 0,
+    income: 0
+}
+let addExpense = function (account, amount) {
+account={}
+//account.expenses =account.expenses + amount
+console.log(account) 
+}
+
+addExpense(myAccount, 2.50)
+
+for (var i = 0; i < i++; i++)
+{
+    console.log(myAccount)
+
+
+}
+console.log(myAccount)
+console.log(myAccount)
+console.log(myAccount)
+console.log(myAccount)
+console.log(myAccount)
+console.log(myAccount)
+console.log("Günaydın herkese, ne haber")
+
+const startt = 22;
+
+let incr = 1;
+
+while (incr < startt) {
+    
+    incr++;
+    console.log("Günaydın " + (startt - incr) + " kaldı");
+}
